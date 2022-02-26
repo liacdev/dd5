10aa0bd966f1782fb85b5b13b6e44d18653846c6
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-02-26 17:57:32 -0600

    Customer delivery package

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("Ouch!");
+    }
+
+     void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Package"){
+        Debug.Log("Got package!");
+        }
+
+        if (other.tag == "Customer"){
+        Debug.Log("Package delivered!");
+        }
+    }
+}
